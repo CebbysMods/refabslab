e95774d5404596064c9a26ddbaca46e5d6b72c72
CebbyS <cebbys.dev@gmail.com>
2021-09-17 17:40:35 +0300

    Added missing files

---
+++ src/main/java/lv/cebbys/mcmods/refabslab/RefabslabClient.java
@@ -0,0 +1,18 @@
+package lv.cebbys.mcmods.refabslab;
+
+import lv.cebbys.mcmods.refabslab.content.models.providers.DoubleSlabModelProvider;
+import lv.cebbys.mcmods.refabslab.content.models.providers.DoubleWallModelProvider;
+import lv.cebbys.mcmods.refabslab.events.RefabslabEventsClient;
+import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
+
+public class RefabslabClient {
+
+	protected static void initResources() {
+
+	}
+	
+	protected static void initProviders() {
+		ModelLoadingRegistry.INSTANCE.registerResourceProvider(rm -> new DoubleSlabModelProvider());
+		ModelLoadingRegistry.INSTANCE.registerResourceProvider(rm -> new DoubleWallModelProvider());
+	}
+}
