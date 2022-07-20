b33b50d0f3d8185add9cdce9c06f9cc38908d6f7
JanisValentinovics <valentinovicskristians@gmail.com>
2022-07-20 18:17:25 +0300

    Updated support for Respro Mandatory Refabslab resource pack is automatically enabled

--- src/main/java/lv/cebbys/mcmods/refabslab/resource/ResourceEntrypoint.java
+++ src/main/java/lv/cebbys/mcmods/refabslab/resource/ResourceEntrypoint.java
@@ -1,9 +1,11 @@
 package lv.cebbys.mcmods.refabslab.resource;
 
-import com.mojang.bridge.game.PackType;
+import lv.cebbys.mcmods.refabslab.Refabslab;
+import lv.cebbys.mcmods.refabslab.content.block.DoubleSlabBlock;
 import lv.cebbys.mcmods.respro.api.ResproRegistry;
-import lv.cebbys.mcmods.respro.imp.component.BlockProperty;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.SharedConstants;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 
 import static lv.cebbys.mcmods.refabslab.Refabslab.MODID;
@@ -11,18 +13,27 @@ import static lv.cebbys.mcmods.refabslab.Refabslab.MODID;
 public class ResourceEntrypoint {
 
     public static void registerAssets() {
-        ResproRegistry.registerAssetPack(new Identifier(MODID, "assets"), pack -> {
-            pack.addPackName("Refabslab Assets");
-            pack.addPackIcon("assets/refabslab/icon.png");
-            pack.addPackMeta(meta -> {
-                meta.packFormat(SharedConstants.getGameVersion().getPackVersion(PackType.RESOURCE));
-                meta.description("Slab assets");
+        ResproRegistry.registerAssets(assets -> {
+            assets.setDumpMode(FabricLoader.getInstance().isDevelopmentEnvironment());
+            assets.setPackId(new Identifier(MODID, "assets"));
+            assets.setPackProfile(profile -> {
+                profile.setAlwaysEnabled(true);
+                profile.setPackName(name -> name.setText("Refabslab Assets"));
+                profile.setPackIcon(icon -> icon.setFromResources(Refabslab.class, "assets/refabslab/icon.png"));
+                profile.setPackMeta(meta -> {
+                    meta.setDescription("Slab Assets");
+                    meta.setFormat(ResourceType.CLIENT_RESOURCES.getPackVersion(SharedConstants.getGameVersion()));
+                });
             });
 
             Identifier modelId = new Identifier(MODID, "block/double_slab_block");
-            pack.addVariantBlockState(new Identifier(MODID, "double_slab_block"), state -> {
+            assets.setVariantBlockstate(new Identifier(MODID, "double_slab_block"), variants -> {
                 for (int i = 0; i < 16; i++) {
-                    state.variant(new BlockProperty("light_level", String.valueOf(i)), v -> v.model(modelId));
+                    Integer lightLevel = i;
+                    variants.setVariant(
+                            property -> property.setProperty(DoubleSlabBlock.LIGHT_LEVEL, lightLevel),
+                            variant -> variant.setModel(modelId)
+                    );
                 }
             });
         });
